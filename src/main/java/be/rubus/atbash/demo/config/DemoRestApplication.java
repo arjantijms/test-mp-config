7546447e15df7711e7e19af77c009e58104a42ef
Arjan Tijms <arjan.tijms@gmail.com>
2022-10-05 08:30:43 +0200

    Reproducer with beans.xml from GF 24125
    https://github.com/eclipse-ee4j/glassfish/issues/24125
    
    Signed-off-by: Arjan Tijms <arjan.tijms@gmail.com>

---
+++ src/main/java/be/rubus/atbash/demo/config/DemoRestApplication.java
@@ -0,0 +1,11 @@
+package be.rubus.atbash.demo.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoRestApplication extends Application {
+}
